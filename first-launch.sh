9b9f13d4f7cd2a2a7064b47804b55761ac9e6b65
Emanuele Sorce <emanuele.sorce@hotmail.com>
2017-12-02 16:07:24 -0500

    First commit

---
+++ first-launch.sh
@@ -0,0 +1,7 @@
+mkdir ~/.cache/iperspace.emanuelesorce/
+cp -v iperspace.tar.gz ~/.cache/iperspace.emanuelesorce/
+
+cd ~/.cache/iperspace.emanuelesorce/
+tar xvzf iperspace.tar.gz
+rm iperspace.tar.gz
+./iperspace
